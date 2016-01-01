5109b995ff17cad32861f53b58dae48e659c3544
AlexOuyang <ouyangchenxing@hotmail.com>
2016-01-01 02:55:41 -0800

    add create cluster of nodes

--- js/pgm.js
+++ js/pgm.js
@@ -2,16 +2,33 @@
     global $:false, jQuery:false, d3:false
 */
 
+// Implement max and min function for array
+Array.max = function (array) {
+    return Math.max.apply(Math, array);
+};
+
+Array.min = function (array) {
+    return Math.min.apply(Math, array);
+};
+
+
 /* This is the probabilistic graphic model */
 
-var graph = function (graphConfiguration) {
+var pgm = function (graphConfiguration) {
     "use strict";
 
-    var graphData = [], // data binds to the graph
-        clusterMat = [], // data specifies the number of nodes each layer
+    var self = this;
+
+    var graphData = {
+            clusterMat: [], // data specifies the number of nodes each layer
+            data: [] // data binds to the graph
+        },
+
         directedPath = [], // directedPath is a list of visited nodes
         config = graphConfiguration || {
-            dim: {
+            transform: {
+                x: 0,
+                y: 0,
                 width: window.innerWidth,
                 height: window.innerHeight - 120
             },
@@ -23,18 +40,10 @@ var graph = function (graphConfiguration) {
             },
             background: {
                 grid: false,
-                color: "none"
+                color: "grey"
             },
             zoom: true,
         },
-        margin = {
-            top: -5,
-            right: -5,
-            bottom: -5,
-            left: -5
-        },
-        width = config.dim.width - margin.left - margin.right,
-        height = config.dim.height - margin.top - margin.bottom,
 
         // Zoom behavior
         zoom = d3.behavior.zoom().scaleExtent([1, 10])
@@ -48,31 +57,38 @@ var graph = function (graphConfiguration) {
         }),
 
         // Dragging nodes behavior
-        drag = d3.behavior.drag()
+        onClick = d3.behavior.drag()
         .origin(function (d) {
             return d;
         })
         .on("dragstart", function (d) {
-            d3.event.sourceEvent.stopPropagation();
-            d3.select(this).classed("dragging", true);
-            var clickedVertexId = parseInt(this.id, 10);
-            traverseGraph(clickedVertexId);
-            drawGraph();
+            // Check if the clicked node is in the first layer
+            // which are the num of nodes in first layer of clusterMat
+            if (this.id < graphData.clusterMat[0]) {
+                d3.event.sourceEvent.stopPropagation();
+                d3.select(this).classed("dragging", true);
+                var clickedVertexId = parseInt(this.id, 10);
+                traverseGraph(clickedVertexId, graphData.data);
+                drawGraph(graphData.data);
 
-            // testing 
-            $('.path strong').text(directedPath);
+                // testing 
+                $('.path strong').text(directedPath);
+            } else {
+                // Else clear the path
+                clearVisitedPath();
+            }
         }),
 
         svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", config.transform.width)
+        .attr("height", config.transform.height)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.right + ")")
+        .attr("transform", "translate(" + config.transform.x + "," + config.transform.y + ")")
         .call(zoom),
 
         rect = svg.append("rect")
-        .attr("width", width)
-        .attr("height", height)
+        .attr("width", config.transform.width)
+        .attr("height", config.transform.height)
         .style("fill", config.background.color)
         .style("pointer-events", "all")
         .on("click", function (d) {
@@ -81,31 +97,31 @@ var graph = function (graphConfiguration) {
 
         container = svg.append("g");
 
-    function dataScreening(graphData) {
+    function dataScreening(data) {
 
         // Verifies if each vertex's id matches its position in the array 
         // and the weights of all adjacent vertices sum to 1;
 
-        if (graphData.length <= 1) {
+        if (data.length <= 1) {
             console.error("input graph data is empty");
             return;
         }
 
         let weightSum = 0;
 
-        for (let vertexIdx = 0; vertexIdx < graphData.length; vertexIdx++) {
-            if (graphData[vertexIdx].id !== vertexIdx) {
+        for (let vertexIdx = 0; vertexIdx < data.length; vertexIdx++) {
+            if (data[vertexIdx].id !== vertexIdx) {
                 console.error("Vertex's id must match its position index in the list of vertices");
                 console.error(vertexIdx + " th element in the list does not match its position index");
                 return;
             }
 
-            var adjVertices = graphData[vertexIdx].adjacentVertex;
+            var adjVertices = data[vertexIdx].adjacentVertex;
             if (adjVertices) {
                 for (let i = 0; i < adjVertices.length; i++) {
                     weightSum += adjVertices[i].weight;
                 }
-                if (weightSum != 1.0) {
+                if (weightSum !== 1.0) {
                     console.error("The sum of a vertex's adjacent vertice's weight must be 1");
                     console.error(vertexIdx + "th element's adjacent vertices's weight does not sum to 1");
                     return;
@@ -116,16 +132,16 @@ var graph = function (graphConfiguration) {
     }
 
 
-    function createEdgesInGraphData(graphData) {
-        // Takes in the graph data, modifies the graphData by adding a list of edges into the graphData and add to self   
-        if (graphData.length <= 1) {
+    function createEdgesInGraphData(data) {
+        // Takes in the graph data, modifies the data by adding a list of edges into the data and add to self   
+        if (data.length <= 1) {
             console.error("input graph data is empty");
             return;
         }
 
-        // Go through each vertex in graphData and add 'edges' attribute to each vertex
-        for (var vertexIdx = 0; vertexIdx < graphData.length; vertexIdx++) {
-            var currentVertex = graphData[vertexIdx];
+        // Go through each vertex in data and add 'edges' attribute to each vertex
+        for (var vertexIdx = 0; vertexIdx < data.length; vertexIdx++) {
+            var currentVertex = data[vertexIdx];
             if (!currentVertex.adjacentVertex) {
                 currentVertex.edges = null;
             } else {
@@ -135,7 +151,7 @@ var graph = function (graphConfiguration) {
                     var targetVertexWeight = currentVertex.adjacentVertex[adjVertexIdx].weight;
                     var edge = {};
                     edge.edgeWeight = targetVertexWeight;
-                    edge.edgeNodes = [currentVertex, graphData[targetVertexId]];
+                    edge.edgeNodes = [currentVertex, data[targetVertexId]];
                     currentVertex.edges.push(edge);
                 }
             }
@@ -143,7 +159,7 @@ var graph = function (graphConfiguration) {
     }
 
 
-    function traverseGraph(vertexId) {
+    function traverseGraph(vertexId, data) {
         // Takes in the id of a node and traverse trough the graph to connect impacted nodes
         // Returns the id of the visited node
 
@@ -166,14 +182,14 @@ var graph = function (graphConfiguration) {
         }
 
         let visitedNodes = [vertexId];
-        let node = graphData[vertexId];
+        let node = data[vertexId];
 
-        while (node.adjacentVertex) {
+        while (node.adjacentVertex != undefined) {
             console.log("Current Vertex: " + vertexId);
             vertexId = chooseRandomAdjVertex(node);
             console.log("Vextex chosen: " + vertexId);
             console.log("--------");
-            node = graphData[vertexId];
+            node = data[vertexId];
             visitedNodes.push(vertexId);
         }
 
@@ -213,7 +229,7 @@ var graph = function (graphConfiguration) {
             });
     }
 
-    function drawVertices() {
+    function drawVertices(data) {
         // clear vertices then redraw all the vertices in the grpah
         d3.selectAll(".vertex").remove();
 
@@ -227,7 +243,7 @@ var graph = function (graphConfiguration) {
             })
             .attr("transform", function (d) {
                 return "translate(" + d.x + "," + d.y + ")";
-            }).call(drag);
+            }).call(onClick);
 
         vertices.append("circle")
             .attr("class", "node")
@@ -250,7 +266,7 @@ var graph = function (graphConfiguration) {
 
     }
 
-    function drawEdges() {
+    function drawEdges(data) {
         // clear edges then redraw all the edges in the graph
         d3.selectAll("path").remove();
 
@@ -299,8 +315,8 @@ var graph = function (graphConfiguration) {
         }
 
         // Draw each vertex's edges based on weight
-        for (let vertexIdx = 0; vertexIdx < graphData.length; vertexIdx++) {
-            let currentVertex = graphData[vertexIdx];
+        for (let vertexIdx = 0; vertexIdx < data.length; vertexIdx++) {
+            let currentVertex = data[vertexIdx];
             if (currentVertex.edges) {
                 for (let edgeIdx = 0; edgeIdx < currentVertex.edges.length; edgeIdx++) {
                     let edgeNodes = currentVertex.edges[edgeIdx].edgeNodes;
@@ -311,8 +327,8 @@ var graph = function (graphConfiguration) {
         }
 
         // Draw the vertex in visitedNodes slowly one by one
-        for (let vertexIdx = 0; vertexIdx < graphData.length; vertexIdx++) {
-            let currentVertex = graphData[vertexIdx];
+        for (let vertexIdx = 0; vertexIdx < data.length; vertexIdx++) {
+            let currentVertex = data[vertexIdx];
             if (currentVertex.edges) {
                 for (let edgeIdx = 0; edgeIdx < currentVertex.edges.length; edgeIdx++) {
                     let edgeNodes = currentVertex.edges[edgeIdx].edgeNodes;
@@ -323,51 +339,89 @@ var graph = function (graphConfiguration) {
         }
     }
 
-    function drawGraph() {
+    function drawGraph(data) {
         // Used to redraw the graph on start and when moving
 
-        drawEdges();
-        drawVertices();
+        drawEdges(data);
+        drawVertices(data);
     }
 
 
     function clearVisitedPath() {
         directedPath = [];
-        drawGraph();
+        drawGraph(graphData.data);
     }
 
 
-    this.bind = function (data) {
+    this.bind = function (gd) {
         // Used to bind the data to the graph and render the graph
 
-        if (data.length <= 1) {
+        if (gd.data.length <= 1) {
             console.error("input graph data is empty");
             return;
         }
 
         // Add the graphData as a class attribute
-        graphData = data;
-        dataScreening(graphData);
-        createEdgesInGraphData(graphData);
-        if (config.background.grid) drawGrid();
-        drawGraph();
+        graphData = gd;
+        dataScreening(gd.data);
+        createEdgesInGraphData(gd.data);
+        if (config.background.grid) {
+            drawGrid();
+        }
+        drawGraph(gd.data);
     };
 
-    this.createCluster = function (clusterMat) {
+
+    this.setAdjacentVertex = function (id, adjVtx) {
+        // Set adjacent vertex for vertex with id 
+        if (!id || !adjVtx) {
+            console.error("setAdjacentVertex(id, adjVtx) params are not satisfied.");
+        }
+
+        graphData.data[id].adjacentVertex = adjVtx;
+    }
+
+    this.createCluster = function (cMat) {
         // Used to create a clusters of nodes based on the cluster matrix
         // Ex of cluster mat [2, 3, 4] creates a cluster of 9 nodes where
         // 2 in first layer, 3 in 2nd layer and 4 in 3rd layer
-        clusterMat = clusterMat;
-        let data = [];
-        let layerPosX = [1];
+        let graphData = {
+            clusterMat: cMat,
+            data: []
+        };
+        let offsetPosX = config.transform.width / (cMat.length + 1); // get the x offset for first node
+        let minPosY = config.transform.height / (Array.max(cMat) + 1); // get the y offset for the layer with the most amount of nodes
 
-        function getLayerPoxX(clusterMat) {
-            let firstNodePosX = config.dim.width / (clusterMat.length + 1);
+        // Data properties: id, x, y, r 
+        let data = [];
+        let id = 0;
+        let x;
+        let y;
+        let r = Array.min([offsetPosX, minPosY]) / 3.0;
+
+        for (let i = 0; i < cMat.length; i++) {
+            // Reset offset Y coordinate for each layer
+            let offSetPosY = config.transform.height / (cMat[i] + 1);
+            for (let j = 0; j < cMat[i]; j++) {
+                x = offsetPosX * (i + 1);
+                y = offSetPosY * (j + 1);
+                data.push({
+                    id: id,
+                    x: x,
+                    y: y,
+                    r: r
+                });
+                id++;
+            }
 
         }
 
-
+        return {
+            clusterMat: cMat,
+            data: data
+        };
     };
+
 };
 
 
